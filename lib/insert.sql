da55aa2fb699e90e78c054308ddb39caecc4de3f
Andrew Pellum <apellum19@gmail.com>
2021-08-20 13:47:57 -0600

    commit tree cc9f34d98030edd525ba3af95af13a1e6d5f6b8b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "F", "Brown", "Feisty", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Black", "Feisty", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "Black", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "F", "Black", "Feisty", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 11, "M", "Brown", "Feisty", True);
